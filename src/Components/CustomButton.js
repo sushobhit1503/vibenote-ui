0cbd42de39bb70e25145313d8dc7f8442507c2f5
1aastha3 <asp13042002@gmail.com>
2023-10-08 14:17:31 +0530

    styling issues

--- src/Components/CustomButton.js
+++ src/Components/CustomButton.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Button, Typography } from '@mui/material';
 
 const customStyles = (theme, color) => ({
-    height: 48,
+    height: 50,
+    width: 'auto',
     borderRadius: 8,
     border: 'none',
     backgroundColor: color,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingRight: '1rem',
-    paddingLeft: '1rem'
+    textAlign: 'center',
 })
 
 
@@ -22,7 +22,7 @@ const CustomButton = ({ text, theme, color, link, onClick }) => {
   }
   return (
     <div>
-      <Button style={customStyles(theme, color)} href={link} onClick={handleSubmit}>
+      <Button style={customStyles(theme, color)} href={link} onClick={handleSubmit} sx={{px: '16px'}}>
         <Typography sx={{color: theme.palette.background.main, textTransform: 'none'}} style={theme.typography.subHeader.bold}>
           {text}
         </Typography>
--- src/Components/UserInput.js
+++ src/Components/UserInput.js
@@ -4,17 +4,19 @@ import SearchIcon from '@mui/icons-material/Search';
 import { Search } from '../svgIcons/search';
 
 const customStyles = (theme) => ({
-  width: 325,
+  width: 'auto',
   height: 50,
   backgroundColor: theme.palette.text.main,
   '& .MuiSvgIcon-root': {
     color: theme.palette.background.main
   },
   color: theme.palette.background.main,
-  fontFamily: theme.typography.subHeader_1.family,
-  fontSize: theme.typography.subHeader_1.size,
-  fontWeight: theme.typography.subHeader_1.light.weight,
-
+  fontFamily: theme.typography.subHeader_1.light.fontFamily,
+  fontSize: theme.typography.subHeader_1.light.fontSize,
+  fontWeight: theme.typography.subHeader_1.light.fontWeight,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '8px'
 })
 
 const UserInput = ({ text, theme, countryCode, icon, onChange }) => {
@@ -29,8 +31,8 @@ const UserInput = ({ text, theme, countryCode, icon, onChange }) => {
         InputProps={{
           startAdornment: (
             <>
-              {icon && <Search style={{ marginRight: "1rem" }} />}
-              {countryCode && <span style={{ marginRight: 8 }}>{countryCode}</span>}
+              {icon && <Search style={{ marginLeft: "-4px", marginRight: "8px" }} />}
+              {countryCode && <span style={{ marginLeft: "-4px", marginRight: "8px" }}>{countryCode}</span>}
             </>
           ),
           sx: customStyles(theme)
